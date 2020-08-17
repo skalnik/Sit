1634df546028dc39dbc1650ae424645bee447923
Mike Skalnik <mike@skalnik.com>
2020-08-16 21:45:39 -0700

    Initial Commit

---
+++ Sit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sit
+//
+//  Created by Mike Skalnik on 8/16/20.
+//  Copyright © 2020 Mike Skalnik. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
