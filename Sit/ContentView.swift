e69c228dab4bc45c4961fde922cad1967e85060e
Mike Skalnik <mike@skalnik.com>
2020-08-18 21:55:50 -0700

    We can now start and stop a timer ⏲

--- Sit/ContentView.swift
+++ Sit/ContentView.swift
@@ -9,27 +9,66 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var timeLeft: String = "00:00:00"
+    @State private var timeLeft: String = "00:00"
+    @State var timer: Timer? = nil
+
+    @State var minutes: Int = 10
+    @State var seconds: Int = 0
+    @State var timeString: String = String(format: "%02d:%02d", 10, 0)
+    @State var timerRunning: Bool = false
+
     let df = DateFormatter()
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
     var body: some View {
         VStack {
-            Text("\(timeLeft)").padding().onReceive(timer) { time in
-                self.df.dateFormat = "hh:mm:ss"
-                self.timeLeft = self.df.string(from: time)
-            }
-            
-            Button(action: startTimer) {
-                Text("Start")
-                .padding(10)
-                .background(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 2))
+            Text("\(timeString)").padding()
+            Button(action: toggleTimer) {
+                if timerRunning {
+                    Text("Stop")
+                    .padding(10)
+                    .background(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 2))
+                } else {
+                    Text("Start")
+                    .padding(10)
+                    .background(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 2))
+                }
             }
         }
     }
 
-    func startTimer() -> Void {
-        // Make timer start instead of autostarting with `#autconnect()`
+    func toggleTimer() -> Void {
+        if self.timerRunning {
+            self.timer?.invalidate()
+            self.timerRunning = false
+            self.timer = nil
+            self.minutes = 10
+            self.seconds = 0
+
+            self.updateTimeString()
+        } else {
+            self.timerRunning = true
+
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { innerTimer in
+                if self.seconds == 0 {
+                    if self.minutes == 0 {
+                        innerTimer.invalidate()
+                        self.timerRunning = false
+                        self.timer = nil
+                    } else {
+                        self.seconds = 59
+                        self.minutes -= 1
+                    }
+                } else {
+                    self.seconds -= 1
+                }
+
+                self.updateTimeString()
+            }
+        }
+    }
+
+    func updateTimeString() -> Void {
+        self.timeString = String(format: "%02d:%02d", self.minutes, self.seconds)
     }
 }
 
